c4eaeca31c60984170b09ef07a0fe4f33aadf4f8
Chelsea Oktaviany <chelseaoktaviany97@gmail.com>
2022-07-20 14:27:49 +0700

    Update footer.php
    Edited years of copyright

--- layout/footer.php
+++ layout/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer">
-    <div><a href="#">Post Dashboard </a> © 2022 chelseaoktaviany.</div>
+    <div><a href="#">Post Dashboard </a> © <?php echo date("Y"); ?> chelseaoktaviany.</div>
     <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI UI Components</a></div>
 </footer>
